e9661f728f7323a7e261aeb15d46e4d934e8d0b9
skrutikovksy <56722590+skrotikovsky@users.noreply.github.com>
2023-03-05 00:21:18 +0500

    Правки по линтеру

--- project/src/pages/head-guest/head-guest component.tsx
+++ project/src/pages/head-guest/head-guest component.tsx
@@ -3,7 +3,7 @@ function HeadGuest() {
     <section className="film-card">
       <div className="film-card__bg">
         {/* <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" /> */}
-        <img src="img/bg-header.jpg" />
+        <img src="img/bg-header.jpg" alt={'header'}/>
       </div>
       <h1 className="visually-hidden">WTW</h1>
       <header className="page-header">
--- project/src/pages/movie-page/movie-page component.tsx
+++ project/src/pages/movie-page/movie-page component.tsx
@@ -89,17 +89,20 @@ function MoviePageComponent(): JSX.Element {
 
               <div className="film-card__text">
                 <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge
-                  Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.</p>
+                  Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&apos;s friend and protege.
+                </p>
 
-                <p>Gustave prides himself on providing first-class service to the hotel's guests, including satisfying
-                  the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies
+                <p>Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying
+                  the sexual needs of the many elderly women who stay there. When one of Gustave&apos;s lovers dies
                   mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her
-                  murder.</p>
-
+                  murder.
+                </p>
                 <p className="film-card__director"><strong>Director: Wes Anderson</strong></p>
-
-                <p className="film-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe
-                  and other</strong></p>
+                <p className="film-card__starring">
+                  <strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe
+                  and other
+                  </strong>
+                </p>
               </div>
             </div>
           </div>
@@ -114,11 +117,13 @@ function MoviePageComponent(): JSX.Element {
             <article className="small-film-card catalog__films-card">
               <div className="small-film-card__image">
                 <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-                     alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
+                  alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"
+                />
               </div>
               <h3 className="small-film-card__title">
                 <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of
-                  Grindelwald</a>
+                  Grindelwald
+                </a>
               </h3>
             </article>
 
@@ -167,4 +172,4 @@ function MoviePageComponent(): JSX.Element {
       </div>
     </>);
 }
-export default MoviePageComponent
+export default MoviePageComponent;
